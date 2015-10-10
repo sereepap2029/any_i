df18f217e0355a7e8faba83791b5691dd6559684
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-10 22:21:36 +0700

    clip path phase1

--- views/content.php
+++ views/content.php
@@ -1,115 +1,109 @@
 <!-- CONTENT ************************** -->
-
-
-  <div class="section s1">
+<div class="section s1">
     <div class="container">
-      <div class="row">
-        <div class="twelve columns">
-          <h4>Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
-          <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
-        </div>
-       <!--
+        <div class="row">
+            <div class="twelve columns">
+                <h4>Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
+                <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
+            </div>
+            <!--
         <div class="one-half column phones">
           <img class="phone" src="images/content/iphone.png">
         </div>
         -->
-      </div>
+        </div>
     </div>
-  </div>
-
-  <div class="section s2">
+</div>
+<div class="section s2">
     <div class="container">
-      <div class="row">
-        <div class="twelve columns">
-          <h4>Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
-          <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
+        <div class="row">
+            <div class="twelve columns">
+                <h4>Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
+                <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-
-  <div class="section s3">
+</div>
+<div class="section s3">
     <div class="container">
-      <div class="row">
-        <div class="four columns">
-          <h2 class="value-multiplier">67%</h2>
-          <h5 class="value-heading">Purchase Increase</h5>
-          <p class="value-description">Percentage of users more likely to purchase on mobile friendly site.</p>
+        <div class="row">
+            <div class="four columns">
+                <h2 class="value-multiplier">67%</h2>
+                <h5 class="value-heading">Purchase Increase</h5>
+                <p class="value-description">Percentage of users more likely to purchase on mobile friendly site.</p>
+            </div>
+            <div class="four columns">
+                <h2 class="value-multiplier">90%</h2>
+                <h5 class="value-heading">Multi-device Users</h5>
+                <p class="value-description">Most of the world accesses the internet on multiple devices.</p>
+            </div>
+            <div class="four columns">
+                <h2 class="value-multiplier">66%</h2>
+                <h5 class="value-heading">Sad Users</h5>
+                <p class="value-description">Percentage of users that are frustrated with page load times.</p>
+            </div>
         </div>
-        <div class="four columns">
-          <h2 class="value-multiplier">90%</h2>
-          <h5 class="value-heading">Multi-device Users</h5>
-          <p class="value-description">Most of the world accesses the internet on multiple devices.</p>
-        </div>
-        <div class="four columns">
-          <h2 class="value-multiplier">66%</h2>
-          <h5 class="value-heading">Sad Users</h5>
-          <p class="value-description">Percentage of users that are frustrated with page load times.</p>
-        </div>
-      </div>
     </div>
-  </div>
-
-  <div class="section s4">
+</div>
+<div class="section s4">
     <div class="container">
-      <div class="row">
-        <div class="twelve columns">
-          <h4>Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
-          <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
+        <div class="row">
+            <div class="twelve columns">
+                <h4>Stop coding non-responsive sites. Let users view sites from anywhere.</h4>
+                <a class="button button-primary" href="http://getskeleton.com">Try Skeleton</a>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-
-  <div class="section s5">
+</div>
+<div class="section s5">
     <div class="container">
-      <h3 class="section-heading">Responsive Images</h3>
-      <p class="section-description">Skeleton images sit easily in grid with .u-max-full-width class. I suggest exploring solution to serving different images based on device size.</p>
-      <div class="row">
-         <div class="four columns">
-          <img class="u-max-full-width" src="images/content/placeholder.png">
+        <h3 class="section-heading">Responsive Images</h3>
+        <p class="section-description">Skeleton images sit easily in grid with .u-max-full-width class. I suggest exploring solution to serving different images based on device size.</p>
+        <div class="row">
+            <div class="four columns">
+                <img class="u-max-full-width" src="images/content/placeholder.png">
+            </div>
+            <div class="four columns">
+                <img class="u-max-full-width" src="images/content/placeholder.png">
+            </div>
+            <div class="four columns">
+                <img class="u-max-full-width" src="images/content/placeholder.png">
+            </div>
+            <div class="twelve columns">
+                <img class="u-max-full-width  s5-1-img" src="images/content/placeholder.png">
+            </div>
         </div>
-         <div class="four columns">
-          <img class="u-max-full-width" src="images/content/placeholder.png">
-        </div>
-        <div class="four columns">
-          <img class="u-max-full-width" src="images/content/placeholder.png">
-        </div>
-        <div class="twelve columns">
-          <img class="u-max-full-width  s5-1-img" src="images/content/placeholder.png">
-        </div>
-      </div>
     </div>
-  </div>
-
-  <div class="section s6">
+</div>
+<div class="section s6">
     <div class="container">
-      <h3 class="section-heading">Blah</h3>
-      <p class="section-description">Skeleton images sit easily in grid with .u-max-full-width class. I suggest exploring solution to serving different images based on device size.</p>
-      <div class="row">
-         <div class="three columns s6-1">
-          <img class="u-max-full-width" src="http://placehold.it/200x200">
+        <h3 class="section-heading">Blah</h3>
+        <p class="section-description">Skeleton images sit easily in grid with .u-max-full-width class. I suggest exploring solution to serving different images based on device size.</p>
+        <div class="row">
+            <div class="three columns s6-1">
+                <img class="u-max-full-width" src="http://placehold.it/200x200">
+            </div>
+            <div class="three columns s6-1">
+                <img class="u-max-full-width" src="http://placehold.it/200x200">
+            </div>
+            <div class="three columns s6-1">
+                <img class="u-max-full-width" src="http://placehold.it/200x200">
+            </div>
+            <div class="three columns s6-1">
+                <img class="u-max-full-width" src="http://placehold.it/200x200">
+            </div>
         </div>
-         <div class="three columns s6-1">
-          <img class="u-max-full-width" src="http://placehold.it/200x200">
-        </div>
-        <div class="three columns s6-1">
-          <img class="u-max-full-width" src="http://placehold.it/200x200">
-        </div>
-        <div class="three columns s6-1">
-          <img class="u-max-full-width" src="http://placehold.it/200x200">
-        </div>
-      </div>
     </div>
-  </div>
-
-
-
-
+</div>
 <!-- END CONTENT ************************** -->
-
+<!--/ SVG /***************************** -->
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0">
+    <defs>
+        <mask id="mask" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">
+           <ellipse cx="130%" cy="100%" rx="50%" ry="0%" fill="white"/>
+        </mask>
+    </defs>
+</svg>
 <!-- SCRIPT ************************** -->
 <script type="text/javascript">
-
 </script>
-
