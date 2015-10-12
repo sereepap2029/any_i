e5dbbc6336a605a23808ead73c1747462b5f96b3
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-13 04:01:51 +0700

    insert nav

--- views/header.php
+++ views/header.php
@@ -3,9 +3,23 @@
     <div class="container">
         <div class="row">
             <div class="twelve columns">
-            <img class="u-max-full-width" src="http://placehold.it/200x50">
+                <img class="u-max-full-width" src="http://placehold.it/200x50">
                 <h4>Dolor sit amet, consectetur adipiscing elit</h4>
             </div>
+        </div>
+    </div>
+    <div class="menu"><i class="fa fa-bars"></i></div>
+    <div class="row">
+        <div class="twelve columns">
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">any</a></li>
+                    <li><a href="#">contact</a></li>
+                    <li><a href="#">th</a></li>
+                    <li><a href="#">en</a></li>
+                </ul>
+            </nav>
         </div>
     </div>
 </div>
