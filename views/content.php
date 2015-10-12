14e6132d26de4147359b6a8f2410d8359343ff4b
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-12 20:34:57 +0700

    add s4-info + s4-tag

--- views/content.php
+++ views/content.php
@@ -85,6 +85,7 @@
                 <div id="owl-desk" class="owl-carousel owl-theme owl-loaded">
                     <div><img src="http://www.placehold.it/1280x840" alt="111">
                         <div class="s4-info">
+                        <div class="s4-tag"><p>anyyy</p></div>
                             <h1>title</h1>
                             <p>blahhhhhhhhhhh</p>
                         </div>
