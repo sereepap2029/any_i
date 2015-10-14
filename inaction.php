dfb2eb38126b8986ca6cb188823d53957b7aa5ca
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-14 21:08:31 +0700

    Finish inaction.php (not fix firefox svg)

--- inaction.php
+++ inaction.php
@@ -32,10 +32,30 @@ ob_start('ob_gzhandler');
                         <script src="js/jquery.superslides.js"></script>
                         <script type="text/javascript" src="js/main.js"></script>
         </div>
-        <!--/ SVG /***************************** -->
+         <!--/ SVG /***************************** -->
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0">
             <defs>
-               
+                <clipPath id="clip_path-head" clipPathUnits="objectBoundingBox">
+                    <ellipse rx=".9" ry="1" cx="0.5" cy="0" />
+                </clipPath>
+                <clipPath id="clip_path" clipPathUnits="objectBoundingBox">
+                    <ellipse rx="1.5" ry="1" cx="0.5" cy="0" />
+                </clipPath>
+                <clipPath id="clip_path-s3" clipPathUnits="objectBoundingBox">
+                    <ellipse rx="2.2" ry="1" cx="0.5" cy="0" />
+                </clipPath>
+                <clipPath id="clip_path-s5" clipPathUnits="objectBoundingBox">
+                    <ellipse rx="2.7" ry="1" cx="0.5" cy="0" />
+                </clipPath>
+                <clipPath id="clip_path-s6" clipPathUnits="objectBoundingBox">
+                    <ellipse rx="1.8" ry="1" cx="0.5" cy="0" />
+                </clipPath>
+                <clipPath id="clip_tag" clipPathUnits="objectBoundingBox">
+                    <polygon points="1 0,0.75 0.5,1 1,0 1,0 0.5,0 0"> </polygon>
+                </clipPath>
+                <clipPath id="clip_path-s4-img" clipPathUnits="objectBoundingBox">
+                    <ellipse rx="1.6" ry="1" cx="0.5" cy="0" />
+                </clipPath>
             </defs>
         </svg>
         <!--/ End SVG /***************************** -->
--- views/inac.php
+++ views/inac.php
@@ -1,3 +1,4 @@
+
 <!-- HEAD CONTENT ************************** -->
 <div class="headcon section s2">
     <div class="container">
@@ -27,7 +28,7 @@
         <h3 class="section-heading">ผลักดันคุณ ไปสู่ฝัน</h3>
         <div class="row">
             <div class="four columns">
-                <img class="u-max-full-width" src="http://placehold.it/150x150">
+                <img class="u-max-full-width" src="http://placehold.it/200x200">
                 <h5 class="value-heading">การสนับสนุน</h5>
                 <p class="value-description">เมื่อคุณมุ่งมั่นทุ่มเทลงทุนในความฝัน any i ก็พร้อมร่วมสนับสนุนให้ให้ธุรกิจ ในฝันของคุณเติบโตและแข็งแรงมากขึ้น จนกลายเป็นความจริง
                     
@@ -35,13 +36,13 @@
                 <a href="#">Read more</a>
             </div>
             <div class="four columns">
-                <img class="u-max-full-width" src="http://placehold.it/150x150">
+                <img class="u-max-full-width" src="http://placehold.it/200x200">
                 <h5 class="value-heading">คำปรึกษา</h5>
                 <p class="value-description">ทีมงานของ any i มาจากการทำงาน ในหลากหลายสาขา และล้วนมี ความรู้ ความเชี่ยวชาญเฉพาะทาง ให้คุณมั่นใจได้ว่าทุกคำแนะนำใน ทุกขั้นตอนของการดำเนินธุรกิจ จะเป็นไปอย่างถูกต้องแม่นยำ</p>
                 <a href="#">Read more</a>
             </div>
             <div class="four columns">
-                <img class="u-max-full-width" src="http://placehold.it/150x150">
+                <img class="u-max-full-width" src="http://placehold.it/200x200">
                 <h5 class="value-heading">แรงบันดาลใจ</h5>
                 <p class="value-description">เรารู้ว่าในการทำงาน เรื่องของใจ สำคัญไม่แพ้สิ่งใด any i พร้อมแชร์ ประสบการณ์และแนวคิดจากการ ทำงานเพื่อสร้างแรงบันดาลใจให้ คุณพร้อมที่จะฝ่าฟันไปให้ถึงเส้นชัย
                 </p>
