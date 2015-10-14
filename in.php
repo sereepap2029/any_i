3fdee9cb31595f31cdf1cc44998740b9cb03b141
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-14 19:10:26 +0700

    inaction phase1

--- in.php
+++ in.php
@@ -5,7 +5,7 @@ ob_start('ob_gzhandler');
     <html lang="en">
 
     <head>
-        <title></title>
+        <title>any i | in action</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
@@ -23,8 +23,9 @@ ob_start('ob_gzhandler');
 
     <body>
         <div id="wrapper">
+        <div id="fullpage"><!-- fullpage -->
             <?php include("views/header.php"); ?>
-                <?php include("views/content.php"); ?>
+                 <?php include("views/inaction.php"); ?>
                     <?php include("views/footer.php"); ?>
                         <script src="js/jquery.fullpage.js"></script>
                         <script src="js/owl.carousel.min.js"></script>
