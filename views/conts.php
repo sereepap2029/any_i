bad6324a3c9d10bf35aa6be9aa039983aeb9e0c6
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-14 21:23:18 +0700

    insert contact.php + overlay bg

---
+++ views/conts.php
@@ -0,0 +1,26 @@
+<!-- HEAD CONTENT ************************** -->
+<div class="headcon section s2">  
+<div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <div class="s2-info">
+                    <div class="s2-tag">
+                        <ul>
+                            <li>
+                                <p>anyyy</p>
+                            </li>
+                            <li>
+                                <div id="redtag"></div>
+                            </li>
+                        </ul>
+                    </div>
+                    <h1>เราจะอยู่เคียงข้าง ในการเดินทางสู่ ความสำเร็จของทุกฝัน </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <img src="images/footer/footer-bg.png" class="end-bg">
+</div>
+<!-- END HEAD CONTENT ************************** -->
