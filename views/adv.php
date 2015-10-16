f6f936d842f032fe522f88baf11894deab539edc
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-16 12:19:50 +0700

    detailed advisor

--- views/adv.php
+++ views/adv.php
@@ -98,15 +98,17 @@
             </div>
         </div>
     </div>
-    <hr>
-    <div class="row">
-        <div class="twelve columns">
-            <div class="adv-btn">
-                <div class="prev"><a href="#"><i class="fa fa-angle-left"></i> Previous</a></div>
-                <div class="next"><a href="#">Next <i class="fa fa-angle-right"></i></a></div>
+    <div class="advcon-foot">
+        <hr>
+        <div class="row">
+            <div class="twelve columns">
+                <div class="adv-btn">
+                    <div class="prev"><a href="#"><i class="fa fa-angle-left"></i> Previous</a></div>
+                    <div class="next"><a href="#">Next <i class="fa fa-angle-right"></i></a></div>
+                </div>
             </div>
+            <img src="images/footer/footer-bg.png" class="end-bg">
         </div>
-        <img src="images/footer/footer-bg.png" class="end-bg">
     </div>
 </div>
 <!--End section ========= -->
