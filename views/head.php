449ad21d5da1d300b157602c7afabe162ddfcbf7
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-20 22:57:18 +0700

    detailed all 3 + insert ?v=1001

--- views/head.php
+++ views/head.php
@@ -3,11 +3,11 @@
 <script type="text/javascript" src="js/jquery-1.11.3.min.js?v=1001"></script>
 <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="css/font-awesome.min.css?v=1001">
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="js/enquire.min.js"></script>
-<link rel="stylesheet" href="css/skeleton.css">
-<link rel="stylesheet" href="css/jquery.fullpage.css">
-<link rel="stylesheet" href="css/owl.carousel.css">
-<link rel="stylesheet" href="css/owl.theme.css">
+<script type="text/javascript" src="js/jquery.easing.1.3.js?v=1001"></script>
+<script type="text/javascript" src="js/enquire.min.js?v=1001"></script>
+<link rel="stylesheet" href="css/skeleton.css?v=1001">
+<link rel="stylesheet" href="css/jquery.fullpage.css?v=1001">
+<link rel="stylesheet" href="css/owl.carousel.css?v=1001">
+<link rel="stylesheet" href="css/owl.theme.css?v=1001">
 <link rel="stylesheet" href="css/style.css">
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
