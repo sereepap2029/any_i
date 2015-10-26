cdf09a680a4d2c481e5586eaeef8fe8db352b91b
Chanala Wilangka <yaicw49@hotmail.com>
2015-10-26 16:33:58 +0700

    detailed inaction

--- views/inac.php
+++ views/inac.php
@@ -26,26 +26,25 @@
 <!-- in action ========================= -->
 <div class="inaction section s3 ">
     <div class="container">
-        <h3 class="section-heading">ผลักดันคุณ ไปสู่ฝัน</h3>
         <div class="row">
             <div class="four columns">
                 <img class="u-max-full-width" src="http://placehold.it/200x200">
-                <h5 class="value-heading">การสนับสนุน</h5>
-                <p class="value-description">เมื่อคุณมุ่งมั่นทุ่มเทลงทุนในความฝัน any i ก็พร้อมร่วมสนับสนุนให้ให้ธุรกิจ ในฝันของคุณเติบโตและแข็งแรงมากขึ้น จนกลายเป็นความจริง
+                <h5 class="value-heading">PAC SolarAire</h5>
+                <p class="value-description">“วันที่เราหยุดไม่ได้เพราะมาไกลแล้ว แต่จะเดินต่อก็ยังหาทางไปไม่ถูก การที่มีคนเดินเข้ามาช่วยชี้ทางให้ มันเหมือนกับเราเห็นทาง และโอกาสใหม่ที่จะไปต่อได้แล้ว”
                     
                 </p>
                 <a href="#">Read more</a>
             </div>
             <div class="four columns">
                 <img class="u-max-full-width" src="http://placehold.it/200x200">
-                <h5 class="value-heading">คำปรึกษา</h5>
-                <p class="value-description">ทีมงานของ any i มาจากการทำงาน ในหลากหลายสาขา และล้วนมี ความรู้ ความเชี่ยวชาญเฉพาะทาง ให้คุณมั่นใจได้ว่าทุกคำแนะนำใน ทุกขั้นตอนของการดำเนินธุรกิจ จะเป็นไปอย่างถูกต้องแม่นยำ</p>
+                <h5 class="value-heading">XXXX XXXXXX</h5>
+                <p class="value-description">“วันที่เราหยุดไม่ได้เพราะมาไกลแล้ว แต่จะเดินต่อก็ยังหาทางไปไม่ถูก การที่มีคนเดินเข้ามาช่วยชี้ทางให้ มันเหมือนกับเราเห็นทาง และโอกาสใหม่ที่จะไปต่อได้แล้ว”</p>
                 <a href="#">Read more</a>
             </div>
             <div class="four columns">
                 <img class="u-max-full-width" src="http://placehold.it/200x200">
-                <h5 class="value-heading">แรงบันดาลใจ</h5>
-                <p class="value-description">เรารู้ว่าในการทำงาน เรื่องของใจ สำคัญไม่แพ้สิ่งใด any i พร้อมแชร์ ประสบการณ์และแนวคิดจากการ ทำงานเพื่อสร้างแรงบันดาลใจให้ คุณพร้อมที่จะฝ่าฟันไปให้ถึงเส้นชัย
+                <h5 class="value-heading">XXXXXXXXX XX</h5>
+                <p class="value-description">“วันที่เราหยุดไม่ได้เพราะมาไกลแล้ว แต่จะเดินต่อก็ยังหาทางไปไม่ถูก การที่มีคนเดินเข้ามาช่วยชี้ทางให้ มันเหมือนกับเราเห็นทาง และโอกาสใหม่ที่จะไปต่อได้แล้ว”
                 </p>
                 <a href="#">Read more</a>
             </div>
