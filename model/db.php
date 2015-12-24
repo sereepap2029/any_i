f08795388867891b499bdfd97ef1c8a234662899
sereepap2029 <sereepap2029@gmail.com>
2015-12-24 11:08:52 +0700

    init Sync with neumerlin demo

--- model/db.php
+++ model/db.php
@@ -1,8 +1,8 @@
 <?php 
 
-	$dbuser = 'root';
-	$dbpass = 'root';
-	$dbh = new PDO('mysql:host=localhost;dbname=any_i', $dbuser, $dbpass);
+	$dbuser = 'any_i';
+	$dbpass = '112244';
+	$dbh = new PDO('mysql:host=202.170.126.247;dbname=any_i', $dbuser, $dbpass);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
