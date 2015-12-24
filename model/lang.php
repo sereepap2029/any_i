cf89068632a4a68660e39af656a33e350a65a5f4
sereepap2029 <sereepap2029@gmail.com>
2015-12-24 16:54:34 +0700

    FInish FIX

--- model/lang.php
+++ model/lang.php
@@ -3,32 +3,32 @@
 // th region
 $any_lang['th']['']="";
 $any_lang['th']['Philosophy_h1']="เพราะ “โอกาส” คือสิ่งที่เราเชื่อมั่น";
-$any_lang['th']['Philosophy_p']="any i ตั้งมั่นที่จะช่วยเหลือเรื่องการให้ โอกาสในการลงทุนกับคนที่มีความฝัน มีความมุ่งมั่นอยากให้ธุรกิจของตัวเอง เติบโตและประสบความสำเร็จ โดยมีเรา เป็นส่วนหนึ่งในการผลักดันให้ฝัน ของคุณเป็นจริง";
+$any_lang['th']['Philosophy_p']="any i ตั้งมั่นที่จะช่วยเหลือเรื่องการให้โอกาสในการลงทุนกับ<br>คนที่มีความฝัน มีความมุ่งมั่น อยากให้ธุรกิจของตัวเองเติบโต<br>และประสบความสำเร็จ โดยมีเราเป็นส่วนหนึ่งในการผลักดัน<br>ให้ฝันของคุณเป็นจริง";
 
 $any_lang['th']['Dreams_h3']="ผลักดันคุณ ไปสู่ฝัน";
 $any_lang['th']['Dreams_support']="การสนับสนุน";
-$any_lang['th']['Dreams_support_des']="เมื่อคุณมุ่งมั่นทุ่มเทลงทุนในความฝัน any i ก็พร้อมร่วมสนับสนุนให้ให้ธุรกิจ ในฝันของคุณเติบโตและแข็งแรงมากขึ้น จนกลายเป็นความจริง";
+$any_lang['th']['Dreams_support_des']="เมื่อคุณมุ่งมั่นทุ่มเทลงทุนใน<br>ความฝัน any i ก็พร้อมร่วมสนับสนุนให้ธุรกิจในฝันของคุณเติบโตและแข็งแรงมากขึ้น จนกลายเป็นความจริง";
 $any_lang['th']['Dreams_advice']="คำปรึกษา";
-$any_lang['th']['Dreams_advice_des']="ทีมงานของ any i มาจากการทำงาน ในหลากหลายสาขา และล้วนมี ความรู้ ความเชี่ยวชาญเฉพาะทาง ให้คุณมั่นใจได้ว่าทุกคำแนะนำใน ทุกขั้นตอนของการดำเนินธุรกิจ จะเป็นไปอย่างถูกต้องแม่นยำ";
+$any_lang['th']['Dreams_advice_des']="ทีมงานของ any i มาจากการทำงานในหลากหลายสาขา และล้วนมีความรู้ ความเชี่ยวชาญเฉพาะทาง ให้คุณมั่นใจได้ว่าทุก<br>คำแนะนำในทุกขั้นตอนของการดำเนินธุรกิจ จะเป็นไปอย่าง<br>ถูกต้องแม่นยำ";
 $any_lang['th']['Dreams_Inspiration']="แรงบันดาลใจ";
-$any_lang['th']['Dreams_Inspiration_des']="เรารู้ว่าในการทำงาน เรื่องของใจ สำคัญไม่แพ้สิ่งใด any i พร้อมแชร์ ประสบการณ์และแนวคิดจากการ ทำงานเพื่อสร้างแรงบันดาลใจให้ คุณพร้อมที่จะฝ่าฟันไปให้ถึงเส้นชัย";
+$any_lang['th']['Dreams_Inspiration_des']="เรารู้ว่าในการทำงาน เรื่องของใจ สำคัญไม่แพ้สิ่งใด any i พร้อมแชร์ประสบการณ์และแนวคิดจากการทำงานเพื่อสร้างแรงบันดาลใจให้คุณพร้อมที่จะฝ่าฟันไปถึง<br>เส้นชัย";
 
 $any_lang['th']['Trusted_Advisor_h3']="เชี่ยวชาญเฉพาะทาง ตอบโจทย์ได้ตรงจุด";
-$any_lang['th']['Trusted_Advisor_des']="any i มีทีมงานที่เชี่ยวชาญใน หลากหลายด้าน สามารถให้คำปรึกษา เฉพาะทางได้สำหรับทุกช่วงการเติบโตของธุรกิจ";
+$any_lang['th']['Trusted_Advisor_des']="any i มีทีมงานที่เชี่ยวชาญในหลากหลายด้าน สามารถให้คำปรึกษาเฉพาะทางได้<br>สำหรับทุกช่วงการเติบโตของธุรกิจ";
 
-$any_lang['th']['inaction_des']="เราจะอยู่เคียงข้าง ในการเดินทางสู่ ความสำเร็จของทุกฝัน";
+$any_lang['th']['inaction_des']="เราจะอยู่เคียงข้างในการเดินทางสู่ความสำเร็จของทุกฝัน";
 
 $any_lang['th']['Business_Partner']="ผู้ร่วมลงทุนของเรา";
 // en region
 $any_lang['en']['']="";
 $any_lang['en']['Philosophy_h1']="Because We Believe in Opportunity";
-$any_lang['en']['Philosophy_p']="any i is dedicated to advising business owners how to reach the next level of success and helping individuals harness ideas and determination into opportunities to make their big dreams come true.";
+$any_lang['en']['Philosophy_p']="any i is dedicated to advising business owners how to reach the next level of success and helping individuals harness ideas and determination into opportunities <br>to make their big dreams come true.";
 
 $any_lang['en']['Dreams_h3']="Encouraging You Towards Your Dreams";
 $any_lang['en']['Dreams_support']="Support";
 $any_lang['en']['Dreams_support_des']="If you are willing to invest capital and energy in your dreams, any i is ready to help your venture grow from the seed of an idea into a robust reality";
 $any_lang['en']['Dreams_advice']="Advice";
-$any_lang['en']['Dreams_advice_des']="The any i team features a wealth of experience from a diversity of industries to guide businesses on the right path to growth and success.";
+$any_lang['en']['Dreams_advice_des']="The any i team features <br>a wealth of experience from <br>a diversity of industries <br>to guide businesses on the right path to growth and success.";
 $any_lang['en']['Dreams_Inspiration']="Inspiration";
 $any_lang['en']['Dreams_Inspiration_des']="When it comes to achieving success in business, enthusiasm is as significant factor as any other. At any i, we are ready to share hard-won experiences and perspectives, keeping you inspired to overcome any challenges.";
 
