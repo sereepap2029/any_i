293c75bb619f4fcad6f77f09ea1e56070c988aa8
sereepap2029 <sereepap2029@gmail.com>
2016-04-19 15:46:56 +0700

    no message

--- model/db.php
+++ model/db.php
@@ -1,9 +1,9 @@
 <?php 
 
-	$dbuser = 'any_i'; // DB Username
-	$dbpass = '112244'; // DB password
+	$dbuser = 'anyicorp_anyi'; // DB Username
+	$dbpass = 'tY2HyaF8'; // DB password
 	$host="localhost"; // DB host
-	$dbName="any_i";// Database name
+	$dbName="anyicorp_anyi";// Database name
 	$dbh = new PDO('mysql:host='.$host.';dbname='.$dbName, $dbuser, $dbpass);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
